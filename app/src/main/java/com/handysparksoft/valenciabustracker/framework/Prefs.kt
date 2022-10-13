b05f68a72679672447477812596510faa6d9c315
David Asensio <davidasensio81@gmail.com>
2022-10-13 11:53:43 +0200

    Shared Preferences V2 (Generics)

--- app/src/main/java/com/handysparksoft/valenciabustracker/framework/Prefs.kt
+++ app/src/main/java/com/handysparksoft/valenciabustracker/framework/Prefs.kt
@@ -2,22 +2,52 @@ package com.handysparksoft.valenciabustracker.framework
 
 import android.content.Context
 import android.content.SharedPreferences
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
 class Prefs constructor(context: Context) {
     private val sharedPrefs: SharedPreferences = context.getSharedPreferences(PREFS_FILENAME, 0)
 
-    var isCountingClicks: Boolean
-        get() = sharedPrefs.getBoolean(KEY_IS_COUNTING_CLICKS, true)
-        set(value) = sharedPrefs.edit().putBoolean(KEY_IS_COUNTING_CLICKS, value).apply()
-
-    var numberOfClicks: Int
-        get() = sharedPrefs.getInt(KEY_NUMBER_OF_CLICKS, 0)
-        set(value) = sharedPrefs.edit().putInt(KEY_NUMBER_OF_CLICKS, value).apply()
+    var isCountingClicks by sharedPrefs.delegate(defaultValue = true)
+    var numberOfClicks by sharedPrefs.delegate(defaultValue = 0)
 
     companion object {
         const val PREFS_FILENAME = "com.handysparksoft.valenciabustracker.framework.prefs"
+    }
+}
 
-        private const val KEY_IS_COUNTING_CLICKS = "key_is_counting_clicks"
-        private const val KEY_NUMBER_OF_CLICKS = "key_number_of_clicks"
+private fun <Type> SharedPreferences.delegate(
+    defaultValue: Type,
+    key: String? = null
+): ReadWriteProperty<Any, Type> {
+    return object : ReadWriteProperty<Any, Type> {
+
+        @Suppress("UNCHECKED_CAST")
+        override fun getValue(thisRef: Any, property: KProperty<*>): Type {
+            val finalKey = key ?: property.name
+            val result: Any? = when (defaultValue) {
+                is String -> getString(finalKey, defaultValue)
+                is Boolean -> getBoolean(finalKey, defaultValue)
+                is Int -> getInt(finalKey, defaultValue)
+                is Long -> getLong(finalKey, defaultValue)
+                is Float -> getFloat(finalKey, defaultValue)
+                else -> throw IllegalArgumentException("This type cannot be read from Preferences")
+            }
+            return result as Type
+        }
+
+        override fun setValue(thisRef: Any, property: KProperty<*>, value: Type) {
+            with(edit()) {
+                val finalKey = key ?: property.name
+                when (value) {
+                    is String -> putString(finalKey, value)
+                    is Boolean -> putBoolean(finalKey, value)
+                    is Int -> putInt(finalKey, value)
+                    is Long -> putLong(finalKey, value)
+                    is Float -> putFloat(finalKey, value)
+                    else -> throw IllegalArgumentException("This type cannot be saved into Preferences")
+                }
+            }.apply()
+        }
     }
 }
